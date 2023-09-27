35c35bb1a72016ddc4b568d6b0ab7dd88a3e3a0b
pawel.nowicki87 <pawel.nowicki87@o2.pl>
2023-09-27 21:32:01 +0200

    Error identity

--- GymManager.MVC/Program.cs
+++ GymManager.MVC/Program.cs
@@ -5,54 +5,67 @@ using GymManager.Infrastructure;
 using GymManager.MVC.Extensions;
 using GymManager.MVC.Middewares;
 using NLog.Web;
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Logging.ClearProviders();
-builder.Logging.SetMinimumLevel(LogLevel.Information);
-builder.Logging.AddNLogWeb();
-
-builder.Services.AddApplication();
-builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.DefineViewLocation(builder.Configuration);
-builder.Services.AddCulture();
-
-builder.Services.AddReCaptcha(builder.Configuration.GetSection("ReCaptcha"));
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using GymManager.Infrastructure.Persistance;
 
 
-
-    // Add services to the container.
-    builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
+internal class Program
 {
-    app.UseInfrastructure(scope.ServiceProvider.GetRequiredService<IApplicationDbContext>(), app.Services.GetService<IAppSettingsService>(),
-        app.Services.GetService<IEmail>());
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Logging.ClearProviders();
+        builder.Logging.SetMinimumLevel(LogLevel.Information);
+        builder.Logging.AddNLogWeb();
+
+        builder.Services.AddApplication();
+        builder.Services.AddInfrastructure(builder.Configuration);
+        builder.Services.DefineViewLocation(builder.Configuration);
+        builder.Services.AddCulture();
+
+        builder.Services.AddReCaptcha(builder.Configuration.GetSection("ReCaptcha"));
+
+
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        var app = builder.Build();
+
+        using (var scope = app.Services.CreateScope())
+        {
+            app.UseInfrastructure(scope.ServiceProvider.GetRequiredService<IApplicationDbContext>(), app.Services.GetService<IAppSettingsService>(),
+                app.Services.GetService<IEmail>());
+        }
+
+
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseMiddleware<ExceptionHandlerMiddleware>();
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseAuthentication(); ;
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.MapRazorPages();
+
+        app.Run();
+    }
 }
-
-
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseMiddleware<ExceptionHandlerMiddleware>();
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
